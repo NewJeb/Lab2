8e3c0e84f8e22fac76244e2d3a4ee6705db30ec7
newjebv <jebesmail5@gmail.com>
2021-11-08 13:05:39 +0300

    12222

--- 3.py
+++ 3.py
@@ -1,10 +1,13 @@
 f1 = 1
 f2 = 1
 n = int(input("Введите число: "))
-print("Фибоначчи: ")
-print('f1:', f1)
-print('f2:', f2)
-while n > 2:
+if n >= 1000:
+ print("Фибоначчи: ")
+ print('f1:', f1)
+ print('f2:', f2)
+ while n > 2:
      f1, f2 = f2, f1 + f2
      print('-', f2)
      n = n - 1
+else:
+     print("Введите 4х значное число!")
